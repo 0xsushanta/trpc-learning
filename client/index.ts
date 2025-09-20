d6d7b834f8d02e719e3ee934935b43e11e414fe0
sushantaruidas <sushantaruidas95@gmail.com>
2025-09-20 16:25:00 +0530

    Enhance TRPC setup with email context and improve createTodo mutation handling

--- client/index.ts
+++ client/index.ts
@@ -1,16 +1,22 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../server/index.js";
-const trpc= createTRPCProxyClient <AppRouter>({
-    links:[
+const trpc = createTRPCProxyClient<AppRouter>({
+    links: [
         httpBatchLink({
-            url:'http://localhost:3000'
+            url: 'http://localhost:3000',
+            async headers() {
+                return {
+                    authorization: "admin123"
+                }
+            }
         })
     ]
 })
-async function main(){
+async function main() {
     let response = await trpc.createTodo.mutate({
-        title:"hey there",
-        descreption: "helo there"
+       title: "hey there",
+       descreption:"helo hello",
+
     })
     console.log(response);
 }
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import { publicProcedure, router } from "./trpc.js";
-import z from "zod";
+import z, { email } from "zod";
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 
 const todoInputtype = z.object({
@@ -8,24 +8,48 @@ const todoInputtype = z.object({
 });
 
 const appRouter = router({
-  createTodo: publicProcedure
+
+    Signup: publicProcedure.input(z.object({
+        email: z.string().email(),
+        password: z.string()
+    })).mutation(async(opts)=>{
+        const email=opts.input.email
+        const password=opts.input.email
+        // validation
+        //db staffs
+
+        let token="ajsjkdld"
+        return{
+            token
+        }
+    }),
+      createTodo: publicProcedure
     .input(todoInputtype)
     .mutation(async (opts) => {
       const title = opts.input.title;
       const descreption = opts.input.descreption;
-       
+       console.log(opts.ctx.email);
         console.log("req hit")
       // db staff
       return {
-        id: "2",
+        id: "1",
       };
     }),
 });
+
 
 export type AppRouter = typeof appRouter;
 
 const server = createHTTPServer({
   router: appRouter,
+  createContext(opts){
+    let authheader=opts.req.headers['authorization']
+    console.log(authheader);
+    // jwt.verify
+    return{
+      email:"sus123"
+    }
+  }
 });
 
 server.listen(3000, () => {
